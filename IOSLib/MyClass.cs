5bf82145ed05efa6d5c8a157d9fd16988cdc1f6d
Matt Ward <matt.ward@xamarin.com>
2016-09-26 14:20:17 +0100

    Add test solution

---
+++ IOSLib/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace IOSLib
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
---
+++ TestPortableAndIOSNuGetPackage/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TestPortableAndIOSNuGetPackage
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
